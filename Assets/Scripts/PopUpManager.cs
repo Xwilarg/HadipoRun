193f8cb07a68b1fed4ae1d5827d556c0d704aace
Xwilarg <xwilarg@yahoo.fr>
2018-01-27 13:23:54 +0100

    Change sprites names

--- Assets/Scripts/PopUpManager.cs
+++ Assets/Scripts/PopUpManager.cs
@@ -3,15 +3,23 @@ using UnityEngine;
 
 public class PopUpManager : MonoBehaviour {
 
-    List<PopUp> popUps;
+    [Tooltip("Intervalle before next popup spam spawn in seconds")]
+    public Vector2 intervalle;
+
+    private List<PopUp> popUps;
+    private float timer;
+    private float refTimer;
 
     private void Start()
     {
         popUps = new List<PopUp>();
+        timer = 0.0f;
+        refTimer = Random.Range(intervalle.x, intervalle.y);
     }
 
     private void Update ()
     {
+        timer += refTimer
 	    foreach (PopUp pu in popUps)
         {
             pu.Update();
