19eb33fb79f033f3e1c6250256a61d940c2d48b1
vburesi <vincent.buresi@epitech.eu>
2018-01-28 00:09:27 +0100

    Move Scripts files into folders

---
+++ Assets/Scripts/Controller/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	[Tooltip("Vitesse du Joueur")]
+	[Range(0, 15)]
+	public float speed;
+
+	private Rigidbody2D rb;
+
+    private PopUpManager popUp;
+
+	// Use this for initialization
+	void Start () 
+	{
+		rb = GetComponent<Rigidbody2D>();
+		popUp = GameObject.FindGameObjectWithTag ("GameManager").GetComponent<PopUpManager> ();
+	}
+
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+
+		Vector2 movement = new Vector2 (moveHorizontal, 0.0f);
+		rb.velocity = new Vector2(Mathf.Lerp(0, moveHorizontal * speed, 0.8f), 0.0f);
+		rb.AddForce (movement * speed);
+	}
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.CompareTag("Item")) {
+			FileController File = other.GetComponent<FileController> ();
+			popUp.AddDownloadingPopup (File.title, File.size);
+            Destroy (other.gameObject);
+        }
+    }
+}
