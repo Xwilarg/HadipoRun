d9237996a25fd95604df5182b42a6d7fbfde4733
Agraael <m.cescutti@hotmail.fr>
2018-01-27 12:12:19 +0100

    creation player

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	[Tooltip("Vitesse du Joueur")]
+	[Range(0, 15)]
+	public float speed;
+	
+	private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () 
+	{
+		rb = GetComponent<Rigidbody2D>();		
+	}
+
+	void FixedUpdate ()
+	{
+		float moveVertical = Input.GetAxis ("Horizontal");
+
+		Vector2 movement = new Vector2 (moveVertical, 0.0f);
+
+		rb.AddForce (movement * speed);
+	}
+}
