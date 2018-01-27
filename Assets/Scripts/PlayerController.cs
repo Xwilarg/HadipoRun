85f5660b88a97f1706675804ce42ee450cd35c52
Agraael <m.cescutti@hotmail.fr>
2018-01-27 12:48:49 +0100

    mouvement du joueur fini

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,21 +5,21 @@ public class PlayerController : MonoBehaviour {
 	[Tooltip("Vitesse du Joueur")]
 	[Range(0, 15)]
 	public float speed;
-	
+
 	private Rigidbody2D rb;
 
 	// Use this for initialization
 	void Start () 
 	{
-		rb = GetComponent<Rigidbody2D>();		
+		rb = GetComponent<Rigidbody2D>();
 	}
 
 	void FixedUpdate ()
 	{
-		float moveVertical = Input.GetAxis ("Horizontal");
+		float moveHorizontal = Input.GetAxis ("Horizontal");
 
-		Vector2 movement = new Vector2 (moveVertical, 0.0f);
-
+		Vector2 movement = new Vector2 (moveHorizontal, 0.0f);
+		rb.velocity = new Vector2(Mathf.Lerp(0, moveHorizontal * speed, 0.8f), 0.0f);
 		rb.AddForce (movement * speed);
 	}
 }
