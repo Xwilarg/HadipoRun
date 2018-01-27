ba34ace0be229195844887b7c9a2d88c09f9b687
vburesi <vincent.buresi@epitech.eu>
2018-01-27 20:42:51 +0100

    Add Focusable behaviour

---
+++ Assets/Scripts/FocusableController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class FocusableController : MonoBehaviour, IPointerDownHandler {
+
+	private RectTransform panel;
+
+	void Awake () {
+		panel = GetComponent <RectTransform> ();  
+	}
+
+	public void OnPointerDown (PointerEventData data) {
+		panel.SetAsLastSibling ();
+	}
+
+}
