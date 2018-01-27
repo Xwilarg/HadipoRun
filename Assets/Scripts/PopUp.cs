d7df4a998c3109086009592dbd23bab4e65e3f64
Xwilarg <xwilarg@yahoo.fr>
2018-01-27 13:12:24 +0100

    Adding popups sprites from Vincent

---
+++ Assets/Scripts/PopUp.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public enum PopUpDownloadType
+{
+
+}
+
+public abstract class PopUp
+{
+    protected PopUp(Vector2 pos)
+    {
+        isInstantiate = false;
+    }
+
+    public abstract void Update();
+
+    public bool isInstantiate { set; get; }
+}
+
+public class PopUpDownload : PopUp
+{
+    public PopUpDownload(Vector2 pos)
+        : base(pos)
+    {
+    }
+
+    public override void Update()
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ Assets/Scripts/PopUpManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class PopUpManager : MonoBehaviour {
+
+    List<PopUp> popUps;
+
+    private void Start()
+    {
+        popUps = new List<PopUp>();
+    }
+
+    private void Update ()
+    {
+	    foreach (PopUp pu in popUps)
+        {
+            pu.Update();
+        }
+	}
+}
