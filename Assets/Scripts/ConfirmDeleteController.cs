bb347f021ca242614c66c03351f3038115ff0108
vburesi <vincent.buresi@epitech.eu>
2018-01-27 21:47:50 +0100

    Edit ConfirmDeletePopUp

---
+++ Assets/Scripts/ConfirmDeleteController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConfirmDeleteController : MonoBehaviour {
+
+	void onClickNo() {
+		Destroy (gameObject);
+	}
+
+	void onClickYes() {
+		ScoreManager sm = GameObject.FindGameObjectWithTag("GameManager").GetComponent<ScoreManager>();
+
+	}
+}
