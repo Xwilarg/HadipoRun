5dab7c59d747eb29374b02825dfdfd1f4a0addb4
MrAchymist <paul.gaston@sfr.fr>
2018-01-27 13:22:24 +0100

    Addin' spawner

---
+++ Assets/Scenes/Object_spawning.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Object_spawning : MonoBehaviour {
+    [Tooltip("Objets to spawn")]
+    public List<GameObject> Spawnable;
+
+    [Tooltip("Range of spawn")] [Range(0, 100)]
+    public float zone;
+    System.Random rand;
+    [Tooltip("Time beetween spawn")]
+    public float SpawnDelay;
+    float currTime;
+
+	// Use this for initialization
+	void Start () {
+        rand = new System.Random();
+        currTime = SpawnDelay;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        int obj = rand.Next(Spawnable.Count);
+        int xAxis = rand.Next(0, (int)zone);
+        Vector2 spawnPlace = new Vector2(xAxis, 0);
+
+        if (currTime <= 0)
+        {
+            Instantiate(Spawnable[obj], spawnPlace, Quaternion.identity);
+            currTime = SpawnDelay;
+        }
+        else
+            currTime -= Time.deltaTime;
+	}
+}
