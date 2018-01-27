d660de66d906d9dee5c1f2fa46a845dc7b69f602
Xwilarg <xwilarg@yahoo.fr>
2018-01-27 21:25:15 +0100

    Add main menu

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour {
+
+	public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadSceneAsync("TestScene");
+    }
+}
