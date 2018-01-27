34c26995dd649c84466d7555fb6f6e8c7e911a5b
Unknown <m.cescutti@hotmail.fr>
2018-01-27 16:11:22 +0100

    new base scene with spwanner, walls and player

--- Assets/Scripts/Object_spawning.cs
+++ Assets/Scripts/Object_spawning.cs
@@ -12,23 +12,26 @@ public class Object_spawning : MonoBehaviour
     public float zone;
     System.Random rand;
     [Tooltip("Time beetween spawn")]
+	[Range(0, 100)]
     public float SpawnDelay;
     float currTime;
 
+	private Collider2D col2D;
     // Use this for initialization 
     void Start()
     {
         rand = new System.Random();
         currTime = SpawnDelay;
+		col2D = GetComponent<Collider2D> ();
     }
 
     // Update is called once per frame 
     void Update()
     {
-        int maxX = rand.Next((int)(GetComponent<Collider2D>().transform.localScale.x * -1), (int)(GetComponent<Collider2D>().transform.localScale.x));
+		int maxX = rand.Next((int)(col2D.transform.localScale.x * -1), (int)col2D.transform.localScale.x);
         int obj = rand.Next(Spawnable.Count);
-        int yAxis = rand.Next(0, (int)zone);
-        Vector2 spawnPlace = new Vector2(maxX, yAxis);
+        //int yAxis = rand.Next(0, (int)zone);
+		Vector2 spawnPlace = new Vector2(maxX, col2D.transform.position.y);
 
         if (currTime <= 0)
         {
