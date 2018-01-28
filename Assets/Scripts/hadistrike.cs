08836bb748e8130e379cda7001e82d0d3a45f1c6
Xwilarg <xwilarg@yahoo.fr>
2018-01-28 14:20:56 +0100

    Add popup from Hadopi

--- Assets/Scripts/hadistrike.cs
+++ Assets/Scripts/hadistrike.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 public class hadistrike : MonoBehaviour {
@@ -11,14 +9,10 @@ public class hadistrike : MonoBehaviour {
         strikes = 0;
         Count_strike.text = strikes.ToString();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 
     public void UpdateStrike()
     {
+        GetComponent<PopUpManager>().AddAlert("Illegal downloading detected", "Warning: illegal operations has been detected from your internet connexion.");
         strikes++;
         Count_strike.text = strikes.ToString();
     }
