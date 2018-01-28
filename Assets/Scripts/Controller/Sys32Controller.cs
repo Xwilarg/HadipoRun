32dd5e388af3bc0682157324e2ab994dcf391cde
vburesi <vincent.buresi@epitech.eu>
2018-01-28 11:58:45 +0100

    Re-add Sys32 window

---
+++ Assets/Scripts/Controller/Sys32Controller.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sys32Controller : MonoBehaviour {
+
+	public void onClickNo() {
+		Destroy (gameObject);
+	}
+
+	public void onClickYes() {
+		ScoreManager sm = GameObject.FindGameObjectWithTag("GameManager").GetComponent<ScoreManager>();
+		sm.gameOverBSOD ();
+	}
+}
