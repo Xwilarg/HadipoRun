b69ae96d7c6e99b4a75e0cc02b58d7f6d3a0f313
Xwilarg <xwilarg@yahoo.fr>
2018-01-28 10:08:13 +0100

    Begin to work on Sniper mouvement

---
+++ Assets/Scripts/MouvementPaterns/SniperMouvement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SniperMouvement : MonoBehaviour {
+
+    float refTime;
+    float currTime;
+    LineRenderer lr;
+	void Start () {
+        refTime = 3.0f;
+        currTime = 0.0f;
+        lr = new LineRenderer();
+        Vector3[] pos = { transform.position, transform.position + Vector3.down * 10 };
+        //lr.SetPositions(pos);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
