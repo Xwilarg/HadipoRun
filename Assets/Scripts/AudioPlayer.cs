8ae7113b932a31ab4c5738dbb8560a8fd8714116
MrAchymist <paul.gaston@sfr.fr>
2018-01-28 08:30:00 +0100

    Audding sound plyaer script

---
+++ Assets/Scripts/AudioPlayer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class AudioPlayer : MonoBehaviour {
+    AudioSource player;
+    AudioClip Aclip;
+
+	// Use this for initialization
+	void Start () {
+        player = GetComponent<AudioSource>();
+        Aclip = new AudioClip();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void Play(string name)
+    {
+        if (File.Exists("Assets/Sounds/" + name))
+        {
+            Aclip = (AudioClip)Resources.Load("Sounds/" + name);
+            player.clip = Aclip;
+            player.Play();
+            Aclip.UnloadAudioData();
+        }
+    }
+}
