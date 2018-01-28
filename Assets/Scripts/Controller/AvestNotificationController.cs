815613c559c90cfb35682801c42a484de9f0b50c
vburesi <vincent.buresi@epitech.eu>
2018-01-28 01:26:02 +0100

    Add Show() to AvestNotificationController

--- Assets/Scripts/Controller/AvestNotificationController.cs
+++ Assets/Scripts/Controller/AvestNotificationController.cs
@@ -4,22 +4,43 @@ using UnityEngine;
 
 public class AvestNotificationController : MonoBehaviour {
 
+	public float delay = 3.0f;
 	public float speed;
 	public float yMin, yMax;
 	private bool isOpen;
+	private bool isShowing;
 	private RectTransform panelRectTransform;
+	private float currentDelay = 0.0f;
 	// Use this for initialization
 	void Start () {
 		isOpen = false;
+		isShowing = false;
 		panelRectTransform = transform as RectTransform;
 		panelRectTransform.localPosition = new Vector2 (panelRectTransform.localPosition.x, yMin);
+
+	}
+
+	void Show() {
+		if (isShowing) {
+			return;
+		} else {
+			currentDelay = 0.0f;
+			isShowing = true;
+			Open ();
+		}	
 	}
 
 	// Update is called once per frame
 	void Update () {
 		if (isOpen) {
 			if (panelRectTransform.localPosition.y < yMax) {
-				panelRectTransform.localPosition = new Vector2(panelRectTransform.localPosition.x, panelRectTransform.localPosition.y + Time.deltaTime * speed);
+				panelRectTransform.localPosition = new Vector2 (panelRectTransform.localPosition.x, panelRectTransform.localPosition.y + Time.deltaTime * speed);
+			} if (isShowing) {
+				currentDelay += Time.deltaTime;
+				if (currentDelay > delay) {
+					isShowing = false;
+					Close ();
+				}
 			}
 		} else {
 			if (panelRectTransform.localPosition.y > yMin) {
