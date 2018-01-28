6dd91ba31b961640183e2a54871b8849225de437
MrAchymist <paul.gaston@sfr.fr>
2018-01-28 13:37:13 +0100

    Add strikes

---
+++ Assets/Scripts/hadistrike.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class hadistrike : MonoBehaviour {
+    public Text Count_strike;
+    public int strikes;
+	// Use this for initialization
+	void Start () {
+        strikes = 0;
+        Count_strike.text = strikes.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void UpdateStrike()
+    {
+        strikes++;
+        Count_strike.text = strikes.ToString();
+    }
+}
